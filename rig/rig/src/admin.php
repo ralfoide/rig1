e3cfcad3863a5110250228e7059c049d70b5d069
ralfoide <ralfoide@32473ccc-1b24-0410-ac99-f74d3c1b6af5>
2002-10-21 07:33:59 +0000

    Admin page which respect themes

--- rig/rig/src/admin.php
+++ rig/rig/src/admin.php
@@ -13,39 +13,30 @@
 
 require_once($dir_install . $dir_src . "common.php");
 require_once($dir_install . $dir_src . "admin_util.php");
+
 enter_login(self_url(), TRUE);
 nocache_headers();
-if ($image)
-	prepare_image($album, $image, $html_admin);
-else
-	prepare_album($album, $html_admin);
 
-admin_perform_before_header(self_url());
+if ($image)
+	rig_prepare_image(-1, $album, $image, $html_admin);
+else
+	rig_prepare_album(-1, $album, $html_admin);
+
+rig_admin_perform_before_header(self_url());
+
+rig_display_header($html_rig_admin);
+rig_display_body();
 
 ?>
-<html>
-<head>
-	<title>
-		<?= $html_rig_admin ?>
-	</title>
-</head>
-
-<body bgcolor="<?= $color_body_bg ?>" text="<?= $color_body_text ?>">
 
 <center>
 
-<table width="100%" bgcolor="<?= $color_header_bg ?>"><tr><td>
-	<center>
-		<h1>
-			<?= $html_rig_admin ?>
-		</h1>
-		<font size="+2"><b>
-			<?= $display_title ?>
-		</b></font>
-	</center>
-</td></tr></table>
-
 <?php
+	rig_display_section("<h1> $html_rig_admin </h1>" .
+						"<font size=\"+2\"><b> $display_title </b></font>",
+						$color_title_bg,
+						$color_title_text);
+
 	display_user_name($rig_adm_user);
 	if ($album)
 	{
@@ -53,18 +44,22 @@ admin_perform_before_header(self_url());
 		<p>
 		<img src="<?= rig_encode_url_link(get_album_preview($current_album, TRUE)) ?>">
 		<br>
-		<?php display_current_album() ?>
+		<font color="<?= $color_index_text ?>">
+			<?php display_current_album() ?>
+		</font>
 		<p>
 <?php
 	}
-	admin_perform_defer();
+	echo "<p>";
+
+	rig_admin_perform_defer();
 ?>
 
 <p>
 	<?= $html_comment_stats ?>
 <br>
 <?php
-	$res = get_preview_info($current_album);
+	$res = rig_admin_get_preview_info($current_album);
 	// Result: array{ 0:nb_files, 1:nb_folders, 2:nb_bytes}
 	printf($html_album_stat, $res[2], $res[0], $res[1]);
 ?>
@@ -72,11 +67,9 @@ admin_perform_before_header(self_url());
 
 
 <p>
-<table width="100%" bgcolor="<?= $color_header_bg ?>"><tr><td>
-	<center><b>
-		<?= $html_actions ?>
-	</b></center>
-</td></tr></table>
+	<?php
+		rig_display_section("<b> $html_actions </b>");
+	?>
 <br>
 <a href="<?= self_url("") . "&admin=mk_all_prev" ?>">
 	<?= $html_mk_previews ?>
@@ -101,17 +94,17 @@ admin_perform_before_header(self_url());
 	{
 ?>
 	<p>
-		<table width="100%" bgcolor="<?= $color_header_bg ?>"><tr><td>
-			<center><b>
-				<?= $html_avail_albums ?>
-			</b></center>
-		</td></tr></table>
+		<?php
+			rig_display_section("<b> $html_avail_albums </b>");
+		?>
 	<br>
-	<table colspan="<?= $pref_nb_col ?>" border="1" cellpadding="5" cellspacing="0">
-		<?php display_album_admin() ?>
-	</table>
-	<hr width="10%">
-		<?php display_current_album() ?>
+		<table colspan="<?= $pref_nb_col ?>" border="1" cellpadding="5" cellspacing="0">
+			<?php rig_admin_display_album() ?>
+		</table>
+		<hr width="10%">
+		<font color="<?= $color_index_text ?>">
+			<?php display_current_album() ?>
+		</font>
 	<br>
 <?php
 	}
@@ -121,11 +114,9 @@ admin_perform_before_header(self_url());
 ?>
 
 	<p>
-		<table width="100%" bgcolor="<?= $color_header_bg ?>"><tr><td>
-			<center><b>
-				<?= $html_avail_prevws ?>
-			</b></center>
-		</td></tr></table>
+		<?php
+			rig_display_section("<b> $html_avail_prevws </b>");
+		?>
 	<br>
 	<font size="-1">
 		<?= $html_comment1 ?>
@@ -133,11 +124,13 @@ admin_perform_before_header(self_url());
 		<!--?= $html_comment2 ?-->
 	</font>
 	<p>
-	<table colspan="<?= $pref_nb_col ?>" border="1" cellpadding="5" cellspacing="0">	<!-- colspan="2" -->
-		<?php display_image_admin() ?>
-	</table>
-	<hr width="10%">
-		<?php display_current_album() ?>
+		<table colspan="<?= $pref_nb_col ?>" border="1" cellpadding="5" cellspacing="0">	<!-- colspan="2" -->
+			<?php rig_admin_display_image() ?>
+		</table>
+		<hr width="10%">
+		<font color="<?= $color_index_text ?>">
+			<?php display_current_album() ?>
+		</font>
 	<p>
 
 <?php
@@ -145,21 +138,18 @@ admin_perform_before_header(self_url());
 ?>
 
 <p>
-	<table width="100%" bgcolor="<?= $color_header_bg ?>"><tr><td>
-		<center><b>
-			<?php echo $html_options ?>
-		</b></center>
-	</td></tr></table>
-	<br>
-		<?= $display_language ?>&nbsp;|&nbsp;<a href="<?= self_url(-1, -1, -1, "lang=$html_symb_lang") ?>"><?= $html_desc_lang ?></a>
-	<br>
-		<?= $html_back_to ?>
-		<a href="<?= self_url("", -1, FALSE) ?>"><?= $display_album_title ?></a>
+	<?php
+		rig_display_options();
+	?>
+	<?= $html_back_to ?>
+	<a href="<?= self_url("", -1, FALSE) ?>"><?= $display_album_title ?></a>
 <p>
 
 <?php
-	insert_credits($credits);
-	insert_footer();
+	rig_display_credits($credits, $phpinfo);
+	rig_display_footer();
+	rig_terminate_db();
+
 ?>
 
 </body>
@@ -167,9 +157,12 @@ admin_perform_before_header(self_url());
 <?php
 //-------------------------------------------------------------
 //	$Log$
+//	Revision 1.4  2002/10/21 07:33:59  ralfoide
+//	Admin page which respect themes
+//
 //	Revision 1.3  2002/10/21 01:53:43  ralfoide
 //	prefixing functions with rig_
-//
+//	
 //	Revision 1.2  2002/10/16 04:48:37  ralfoide
 //	Version 0.6.2.1
 //	

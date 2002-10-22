dd73564040a610aff19bcac3a8584a1a49ad8419
ralfoide <ralfoide@32473ccc-1b24-0410-ac99-f74d3c1b6af5>
2002-10-22 22:32:03 +0000

    Global gamma can be changed in the site-specific pref file

--- rig/htdocs/album-sample/prefs.php
+++ rig/htdocs/album-sample/prefs.php
@@ -13,20 +13,24 @@
 // be overrided here.
 
 
-# login options
+// login options
 $pref_allow_guest		= TRUE;				// can be TRUE (default) or FALSE
 $pref_auto_guest		= TRUE;				// should guest authentificate? TRUE (default) or FALSE
 
-$pref_global_gamma		= 1.0;	// use 1.0 for no-op
+// global gamma override
+$pref_global_gamma		= 1.0;	            // use 1.0 for no-op
 
 
 // end
 
 //-------------------------------------------------------------
 //	$Log$
+//	Revision 1.3  2002/10/22 22:32:03  ralfoide
+//	Global gamma can be changed in the site-specific pref file
+//
 //	Revision 1.2  2002/10/21 01:56:53  ralfoide
 //	Added local override of gamma
-//
+//	
 //	Revision 1.1  2002/08/04 00:58:08  ralfoide
 //	Uploading 0.6.2 on sourceforge.rig-thumbnail
 //	

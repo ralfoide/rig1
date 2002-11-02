1ed9729a4fcc812dd3c0074b77beaa71a3ec01af
ralfoide <ralfoide@32473ccc-1b24-0410-ac99-f74d3c1b6af5>
2002-11-02 04:09:09 +0000

    Fixes by Pedro del Gallego and Roberto Francisco (sf.net)

--- rig/rig/src/str_sp.php
+++ rig/rig/src/str_sp.php
@@ -7,7 +7,18 @@
 // $Id$
 //**********************************************
 
-// Affichage en Espanol pour RIG
+// Spanish strings for RIG
+
+// Cambios:
+// 20021101 - Fixes by Pedro del Gallego and Roberto Francisco (sf.net)
+
+// Vocabulario:
+// byte: octeto ?
+// file: fichero
+// folder: carpeta /  carpetas electr&oacute;nicas ?
+// click on image: pulse sobre la imagen / haga click en la imagen ?
+// PHP web server: servidor PHP
+
 
 
 // HTML encoding
@@ -32,11 +43,11 @@ $html_desc_lang		= array('en' => 'English',
 // Themes available
 //-----------------
 
-$html_theme			= 'Color de pagina:';
+$html_theme			= 'Color de p&aacute;gina:';
 $html_desc_theme	= array('blue'  => 'Azul',
 							'sand'  => 'Arena',
 							'khaki' => 'Caqui',
-							'egg'	=> 'Amarillo',
+							'egg'	=> 'Amarilla',
 							'none'	=> 'Ninguna');
 
 
@@ -44,34 +55,34 @@ $html_desc_theme	= array('blue'  => 'Azul',
 //-------------
 
 $html_current_album	= 'Album Actual';
-$html_albums		= 'Albums Dispon&igrave;bles';
-$html_images		= 'Images';
-$html_options		= 'Optiones';
+$html_albums		= 'Albums Disponibles';
+$html_images		= 'Im&aacute;genes';																// rf
+$html_options		= 'Opciones';																		// pdg, rf
 
-$html_generated		= 'Creado en [time] secondos el <i>[date]</i> por <i>[rig-version]</i>';
+$html_generated		= 'Creado en [time] secondos el dia <i>[date]</i> por <i>[rig-version]</i>';		// pdg
 
 $html_admin_intrfce	= 'Interfaz de administraci&oacute;n';
 
 $html_rig_admin		= 'Interfaz de administraci&oacute;n de RIG'; 
-$html_comment_stats	= 'Estad&iacute;sticas par este &aacute;lbum y sub-&aacute;lbums :';
-$html_album_stat	= '[bytes] octets occupados por [files] ficheros en [folders] carpetas electr&oacute;nicas';
-$html_actions		= 'Actiones';
+$html_comment_stats	= 'Estad&iacute;sticas par este &aacute;lbum y sub-&aacute;lbumes :';				// rf
+$html_album_stat	= '[bytes] bytes ocupados por [files] ficheros en [folders] carpetas';				// rf
+$html_actions		= 'Acciones';																		// pdg, rf
 $html_mk_previews	= 'Crear todas las diapos&iacute;tivas';
 $html_rm_previews	= 'Borrar todas las diapos&iacute;tivas';
 $html_rand_previews	= 'Cambiar al azar el icono del &aacute;lbum';
-$html_rename_canon	= 'Retitule los ficheros electr&oacute;nicos 100-1234_img.jpg de Canon';
+$html_rename_canon	= 'Renombrar los ficheros 100-1234_img.jpg de Canon';								// pdg, rf
 $html_use_as_icon	= 'Usar como icono del &aacute;lbum';
-$html_rename_image	= 'Retitule la imagen';
+$html_rename_image	= 'Renombrar la imagen';															// rf
 $html_set_desc		= 'Cambiar la descripci&oacute;n';
 $html_hide_album	= 'Ocultar el &aacute;lbum';
 $html_show_album	= 'Mostrar el &aacute;lbum ';
-$html_avail_albums	= 'Albums disponibles';
+$html_avail_albums	= 'Albumes disponibles';															// rf
 $html_avail_prevws	= 'Diapos&iacute;tivas disponibles';
 $html_comment1		= 'Puede necesitarse recargar esta p&aacute;gina para ver las verdaderas im&aacute;genes.';
-$html_comment2		= 'Chasque encendido las im&aacute;genes para tener acceso a opciones imagen-espec&iacute;ficas.';
-$html_back_to		= 'Para volver al';
-$html_back_album	= 'Para volver al &aacute;lbum';
-$html_back_previous	= 'Para volver al &aacute;lbum anterior';
+$html_comment2		= 'Pulse sobre las im&aacute;genes para tener acceso a opciones imagen-espec&iacute;ficas.';	// rf
+$html_back_to		= 'Volver al';																		// rf
+$html_back_album	= 'Volver al &aacute;lbum';
+$html_back_previous	= 'Volver al &aacute;lbum anterior';
 $html_hidden		= 'ocultado';
 $html_vis_on		= 'Mostrar';
 $html_vis_off		= 'Ocultar';
@@ -79,14 +90,14 @@ $html_vis_off		= 'Ocultar';
 $html_credits		= 'Cr&eacute;ditos';
 $html_show_credits	= 'Mostrar los cr&eacute;ditos de RIG y PHP';
 $html_hide_credits	= 'Ocultar los cr&eacute;ditos';
-$html_text_credits	= '<a href=\"http://rig.powerpulsar.com\">RIG</a> &copy; 2001 por R\'alf<br>';
-$html_text_credits .= 'RIG se distribuye bajo los t&eacute;rminos de la <a href=\"LICENSE.html\">licencia RIG</a>.<br>';
-$html_text_credits .= 'Basado en <a href=\"http://www.php.net\">PHP</a> y ';
-$html_text_credits .= '<a href=\"ftp://ftp.uu.net/graphics/jpeg\">JpegLib</a>.<br>';
+$html_text_credits	= '<a href="http://rig.powerpulsar.com">RIG</a> &copy; 2001 por R\'alf<br>';
+$html_text_credits .= 'RIG se distribuye bajo los t&eacute;rminos de la <a href="LICENSE.html">licencia RIG</a>.<br>';
+$html_text_credits .= 'Basado en <a href="http://www.php.net">PHP</a> y ';
+$html_text_credits .= '<a href="ftp://ftp.uu.net/graphics/jpeg">JpegLib</a>.<br>';
 
-$html_phpinfo		= 'Informationes del server PHP';
-$html_show_phpinfo	= 'Mostrar las informaciones del server PHP';
-$html_hide_phpinfo	= 'Ocultar las informaciones del server PHP';
+$html_phpinfo		= 'Informationes del servidor PHP';
+$html_show_phpinfo	= 'Mostrar las informaciones del servidor PHP';
+$html_hide_phpinfo	= 'Ocultar las informaciones del servidor PHP';
 
 $html_login			= 'Login';
 $html_validate		= 'Validar';
@@ -145,9 +156,12 @@ $pref_date_YMD      = 'D-M-Y';          // Long format.  Must contain D & M & Y.
 
 //-------------------------------------------------------------
 //	$Log$
+//	Revision 1.4  2002/11/02 04:09:09  ralfoide
+//	Fixes by Pedro del Gallego and Roberto Francisco (sf.net)
+//
 //	Revision 1.3  2002/10/23 16:01:00  ralfoide
 //	Added <html lang>; now transmitting charset via http headers.
-//
+//	
 //	Revision 1.2  2002/10/23 08:41:03  ralfoide
 //	Fixes for internation support of strings, specifically Japanese support
 //	

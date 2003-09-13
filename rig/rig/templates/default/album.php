9fbc261bd653deacd11d739f3c14bc5954fb5f67
ralfoide <ralfoide@32473ccc-1b24-0410-ac99-f74d3c1b6af5>
2003-09-13 21:48:45 +0000

    Experimental: template system

---
+++ rig/rig/templates/default/album.php
@@ -0,0 +1,18 @@
+<?php
+//-----------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------
+// end
+
+//-------------------------------------------------------------
+//	$Log$
+//	Revision 1.1  2003/09/13 21:48:45  ralfoide
+//	Experimental: template system
+//
+//-------------------------------------------------------------
+
+// IMPORTANT: the "? >" must be the LAST LINE of this file, otherwise
+// some HTTP output will be started by PHP4 and setting headers or cookies
+// will fail with a PHP error message.
+?>
